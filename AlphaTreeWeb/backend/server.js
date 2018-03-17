2f197d7c5a55802926e2c2b5863d47273d2637f9
mohamed aly anda ndiaye <mohamedalyandandiaye@gmail.com>
2018-03-17 19:32:48 +0100

    add backend & nodemon + mongoose

---
+++ AlphaTreeWeb/backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+//add  midlerWare
+app.use(bodyParser.json());
+
+const api =  express.Router();
+api.get('/test',function(req,res){
+    res.json({message : 'hello'});
+
+});
+
+app.use('/',api);
+
+
+
+const listeningPort = 4201;
+app.listen( listeningPort ,() => {
+    console.log('vous ecoutez  lz  port : '+listeningPort);
+});
